dc90d8bf3953445db890bff1de4fdbc53472445f
Jenny <jenny@Jennys-MacBook-Pro.local>
2022-08-02 14:18:31 -0600

    first commit

---
+++ routes/messages.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const router = new express.Router();
+const ExpressError = require('../expressError');
+const db = require('../db');
+const Message = require('../models/message');
+const { ensureLoggedIn } = require('../../node-practice/node-authentication/middleware/auth');
+
+
+/** GET /:id - get detail of message.
+ *
+ * => {message: {id,
+ *               body,
+ *               sent_at,
+ *               read_at,
+ *               from_user: {username, first_name, last_name, phone},
+ *               to_user: {username, first_name, last_name, phone}}
+ *
+ * Make sure that the currently-logged-in users is either the to or from user.
+ *
+ **/
+
+router.get('/:id', ensureLoggedIn, async (req, res, next) => {
+    try{
+        const {id} = req.params;
+        const results = await Message.get(id);
+        return res.json({message: results});
+    }catch(e){
+        return next(e);
+    }
+})
+
+
+/** POST / - post message.
+ *
+ * {to_username, body} =>
+ *   {message: {id, from_username, to_username, body, sent_at}}
+ *
+ **/
+
+router.post('/', async (req, res, next) => {
+    try{
+        const { to_username, body } = req.body;
+        const results = await Message.create(req.user, to_username, body);
+        return res.json({message: results});
+    }catch(e){
+        return next(e);
+    }
+})
+
+/** POST/:id/read - mark message as read:
+ *
+ *  => {message: {id, read_at}}
+ *
+ * Make sure that the only the intended recipient can mark as read.
+ *
+ **/
+
+router.post('/:id/read', ensureLoggedIn, async (req, res, next) => {
+    try{
+        const {id} = req.params;
+        const message = Message.markRead(id);
+        return res.json({message: message});
+    }catch(e){
+        return next(e);
+    }
+})
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,79 @@
+const express = require('express');
+const router = new express.Router();
+const ExpressError = require('../expressError');
+const db = require('../db');
+const User = require('../models/user');
+
+
+/** GET / - get list of users.
+ *
+ * => {users: [{username, first_name, last_name, phone}, ...]}
+ *
+ **/
+
+router.get('/', async (req, res, next) => {
+    try{
+        const results = await (await User.all()).map(u => (u));
+        return res.json({users: results})
+    }catch(e) {
+        return next(e);
+    }
+})
+
+/** GET /:username - get detail of users.
+ *
+ * => {user: {username, first_name, last_name, phone, join_at, last_login_at}}
+ *
+ **/
+
+router.get('/:username', async (req, res, next) => {
+    try{
+        const {username}= req.params
+        const results = await User.get(username);
+        return res.json({user: results});
+    }catch(e){
+        return next(e);
+    }
+})
+
+/** GET /:username/to - get messages to user
+ *
+ * => {messages: [{id,
+ *                 body,
+ *                 sent_at,
+ *                 read_at,
+ *                 from_user: {username, first_name, last_name, phone}}, ...]}
+ *
+ **/
+
+router.get('/:username/to', async (req, res, next) => {
+    try{
+        const {username} = req.params;
+        const results = await User.messagesTo(username);
+        return {messages: results}
+    }catch(e){
+        return next(e);
+    }
+})
+
+/** GET /:username/from - get messages from user
+ *
+ * => {messages: [{id,
+ *                 body,
+ *                 sent_at,
+ *                 read_at,
+ *                 to_user: {username, first_name, last_name, phone}}, ...]}
+ *
+ **/
+
+ router.get('/:username/from', async (req, res, next) => {
+    try{
+        const {username} = req.params;
+        const results = await User.messagesFrom(username);
+        return {messages: results}
+    }catch(e){
+        return next(e);
+    }
+})
+
+module.exports = router;
